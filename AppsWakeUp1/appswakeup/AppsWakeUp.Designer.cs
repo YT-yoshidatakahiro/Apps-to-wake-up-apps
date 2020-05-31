a2fb66a34d93d8e87b6f0b5fbffe4f5e219f86d5
YT-yoshidatakahiro <63303848+YT-yoshidatakahiro@users.noreply.github.com>
2020-05-31 23:49:43 +0900

    Add files via upload

---
+++ AppsWakeUp1/appswakeup/AppsWakeUp.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace appswakeup
+{
+    partial class AppsWakeUp
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWake = new System.Windows.Forms.Button();
+            this.cmbAppList = new System.Windows.Forms.ComboBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tbSerch = new System.Windows.Forms.TextBox();
+            this.btnSerchStart = new System.Windows.Forms.Button();
+            this.tbfile = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.cmbResult = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnWake
+            // 
+            this.btnWake.Font = new System.Drawing.Font("メイリオ", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnWake.Location = new System.Drawing.Point(600, 1);
+            this.btnWake.Name = "btnWake";
+            this.btnWake.Size = new System.Drawing.Size(78, 41);
+            this.btnWake.TabIndex = 1;
+            this.btnWake.TabStop = false;
+            this.btnWake.Text = "起動";
+            this.btnWake.UseVisualStyleBackColor = true;
+            this.btnWake.Click += new System.EventHandler(this.btnWake_Click);
+            // 
+            // cmbAppList
+            // 
+            this.cmbAppList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAppList.Font = new System.Drawing.Font("メイリオ", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbAppList.FormattingEnabled = true;
+            this.cmbAppList.Location = new System.Drawing.Point(22, 1);
+            this.cmbAppList.Name = "cmbAppList";
+            this.cmbAppList.Size = new System.Drawing.Size(572, 39);
+            this.cmbAppList.TabIndex = 2;
+            this.cmbAppList.TabStop = false;
+            this.cmbAppList.SelectedIndexChanged += new System.EventHandler(this.cmbAppList_SelectedIndexChanged);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("メイリオ", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClose.Location = new System.Drawing.Point(684, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 41);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.TabStop = false;
+            this.btnClose.Text = "終了";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tbSerch
+            // 
+            this.tbSerch.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbSerch.Location = new System.Drawing.Point(22, 90);
+            this.tbSerch.Name = "tbSerch";
+            this.tbSerch.Size = new System.Drawing.Size(762, 39);
+            this.tbSerch.TabIndex = 4;
+            this.tbSerch.Text = "C:\\直下のディレクトリ名";
+            this.tbSerch.TextChanged += new System.EventHandler(this.tbSerch_TextChanged);
+            // 
+            // btnSerchStart
+            // 
+            this.btnSerchStart.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnSerchStart.Location = new System.Drawing.Point(605, 45);
+            this.btnSerchStart.Name = "btnSerchStart";
+            this.btnSerchStart.Size = new System.Drawing.Size(179, 39);
+            this.btnSerchStart.TabIndex = 6;
+            this.btnSerchStart.TabStop = false;
+            this.btnSerchStart.Text = "検索開始";
+            this.btnSerchStart.UseVisualStyleBackColor = true;
+            this.btnSerchStart.Click += new System.EventHandler(this.btnSerchStart_Click);
+            // 
+            // tbfile
+            // 
+            this.tbfile.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbfile.Location = new System.Drawing.Point(22, 135);
+            this.tbfile.Name = "tbfile";
+            this.tbfile.Size = new System.Drawing.Size(762, 39);
+            this.tbfile.TabIndex = 8;
+            this.tbfile.Text = "拡張子名（不明可）";
+            this.tbfile.TextChanged += new System.EventHandler(this.tbfile_TextChanged);
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("メイリオ", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnClick.Location = new System.Drawing.Point(22, 46);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(572, 38);
+            this.btnClick.TabIndex = 9;
+            this.btnClick.TabStop = false;
+            this.btnClick.Text = "ディレクトリ不明：クリック";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // cmbResult
+            // 
+            this.cmbResult.Font = new System.Drawing.Font("メイリオ", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbResult.FormattingEnabled = true;
+            this.cmbResult.Location = new System.Drawing.Point(22, 180);
+            this.cmbResult.Name = "cmbResult";
+            this.cmbResult.Size = new System.Drawing.Size(762, 39);
+            this.cmbResult.TabIndex = 10;
+            this.cmbResult.TabStop = false;
+            this.cmbResult.SelectedIndexChanged += new System.EventHandler(this.cmbResult_SelectedIndexChanged);
+            // 
+            // AppsWakeUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(796, 235);
+            this.Controls.Add(this.cmbResult);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.tbfile);
+            this.Controls.Add(this.btnSerchStart);
+            this.Controls.Add(this.tbSerch);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.cmbAppList);
+            this.Controls.Add(this.btnWake);
+            this.Name = "AppsWakeUp";
+            this.Text = "アプリ起動";
+            this.Load += new System.EventHandler(this.AppsWakeUp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnWake;
+        private System.Windows.Forms.ComboBox cmbAppList;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox tbSerch;
+        private System.Windows.Forms.Button btnSerchStart;
+        private System.Windows.Forms.TextBox tbfile;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.ComboBox cmbResult;
+    }
+}
+
---
+++ AppsWakeUp1/appswakeup/AppsWakeUp.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace appswakeup
+{
+    public partial class AppsWakeUp : Form
+    {
+
+        /// <summary>
+        ///iniファイル読み込み設定
+        /// </summary>
+        [DllImport("kernel32.dll",
+                EntryPoint = "GetPrivateProfileStringW",
+                CharSet = CharSet.Unicode,
+                SetLastError = true)]
+                static extern uint GetPrivateProfileString(
+                string lpAppName,
+                string lpKeyName,
+                string lpDefault,
+                StringBuilder lpReturnedString,
+                uint nSize,
+                string lpFileName);
+
+        private String fln1 = @"./appssetup.ini";
+
+        private string[] appname1 = new string[1];
+        private string[] apppath1 = new string[1];
+
+        /// <summary>
+        /// 当アプリのコンストラクタ
+        /// </summary>
+        public AppsWakeUp()
+        {
+            InitializeComponent();
+
+            cmbResult.Text = "検索結果";
+
+            cmbAppList.Click += new EventHandler(cmbAppList_Click);
+            tbSerch.Click += new EventHandler(tbSerch_Click);
+            tbfile.Click += new EventHandler(tbfile_Click);
+
+            //ウィンドウの右上のバツボタンなど非表示
+            ControlBox = false;
+
+            if (!(System.IO.File.Exists(fln1)))
+            {
+                //起動ファイルのない場合の処理
+                MessageBox.Show("起動アプリリストがありません", "読込不可");
+
+                //終了ボタン以外操作不可
+                btnWake.Enabled = false;
+
+                cmbAppList.Enabled = false;
+
+            }
+            //iniファイルの有無
+            else if (System.IO.File.Exists(fln1))
+            {
+
+                //終了以外のボタン操作可
+                btnWake.Enabled = true;
+
+                cmbAppList.Enabled = true;
+
+                fln1 = @"./appssetup.ini";
+
+                //iniファイル読込処理開始
+                int capacitySize = 256;
+
+                StringBuilder sb = new StringBuilder(capacitySize);
+
+                //コンボボックスへの読み込み
+                //iniファイルの登録分，動的に配列を増減させる
+                for (int j = 0; j < appname1.Length; j++)
+                {
+
+                    if (j >= appname1.Length)
+                    {
+                        MessageBox.Show("アプリ登録数とリスト登録数の不一致", "読込不可");
+
+                        btnWake.Enabled = false;
+
+                        cmbAppList.Enabled = false;
+
+                    }
+                    else
+                    {
+                        //アプリ名の設定
+                        GetPrivateProfileString("listname", string.Format("name" + j), "", sb, Convert.ToUInt32(sb.Capacity), fln1);
+
+                        appname1[j] = @sb.ToString();
+
+                        if (!(appname1[j].Equals("")))
+                        {
+                            cmbAppList.Items.Add(appname1[j]);
+                            Array.Resize(ref appname1, appname1.Length + 1);
+                            //Array.Sort(appname1);
+                        }
+
+                        //アプリパスの設定
+                        GetPrivateProfileString("apppath", string.Format("app" + j), "", sb, Convert.ToUInt32(sb.Capacity), fln1);
+
+                        //stringUTF8に何らかUTF8の文字列が入ってくる
+                        apppath1[j] = @sb.ToString();
+                        if (!(apppath1[j].Equals("")))
+                        {
+                            Array.Resize(ref apppath1, apppath1.Length + 1);
+
+                        }
+                        cmbAppList.SelectedIndex = 0;
+                    }
+                }
+            }
+            //iniファイル読込処理終了
+        }
+
+        /// <summary>
+        /// アプリ終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// 選択アプリの起動ボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnWake_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 1; i < appname1.Length; i++)
+                {
+                    if (cmbAppList.Text == appname1[i])
+                    {
+                        ProcessStartInfo pInfo = new ProcessStartInfo();
+                        pInfo.FileName = apppath1[i];
+
+                        Process p = Process.Start(pInfo);
+
+                        p.WaitForExit();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("開けません", "パスの確認");
+            }
+        }
+
+        /// <summary>
+        /// 起動アプリ選択・登録（登録後，更新）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbAppList_Click(object sender, EventArgs e)
+        {
+            cmbAppList.Items.Clear();
+
+            //iniファイル読込処理開始
+            int capacitySize = 256;
+
+            StringBuilder sb = new StringBuilder(capacitySize);
+
+            for (int j = 0; j < appname1.Length; j++)
+            {
+                //アプリ名の設定
+                GetPrivateProfileString("listname", string.Format("name" + j), "", sb, Convert.ToUInt32(sb.Capacity), fln1);
+
+                appname1[j] = @sb.ToString();
+
+                if (!(appname1[j].Equals("")))
+                {
+                    cmbAppList.Items.Add(appname1[j]);
+                    Array.Resize(ref appname1, appname1.Length + 1);
+                }
+
+                //アプリパスの設定
+                GetPrivateProfileString("apppath", string.Format("app" + j), "", sb, Convert.ToUInt32(sb.Capacity), fln1);
+
+                //stringUTF8に何らかUTF8の文字列が入ってくる
+                apppath1[j] = @sb.ToString();
+                if (!(apppath1[j].Equals("")))
+                {
+                    Array.Resize(ref apppath1, apppath1.Length + 1);
+
+                }
+
+                cmbAppList.SelectedIndex = 0;
+            }
+        }
+
+        /// <summary>
+        /// 登録したいアプリの検索
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSerchStart_Click(object sender, EventArgs e)
+        {
+            //C直下のファイル検索
+            string serchpath = @"C:\";
+
+            try
+            {
+                //ディレクトリのみしかわからない場合
+                if (tbfile.Text == ".拡張子名（不明可）" || tbfile.Text.Equals(""))
+                {
+                    string[] filepath = Directory.GetFiles(serchpath + tbSerch.Text, tbfile.Text, SearchOption.AllDirectories);
+
+                    for (int i = 0; i < filepath.Length; i++)
+                    {
+                        cmbResult.Items.Add(filepath[i]);
+                    }
+                }
+                //ファイル名＋拡張子が判明する場合
+                else
+                {
+                    string[] filepath = Directory.GetFiles(serchpath + tbSerch.Text, "*." + tbfile.Text, SearchOption.AllDirectories);
+
+                    for (int i = 0; i < filepath.Length; i++)
+                    {
+                        cmbResult.Items.Add(filepath[i]);
+                    }
+                }
+            }
+            catch (Exception ignored)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 検索ボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbSerch_Click(object sender, EventArgs e)
+        {
+            cmbResult.Items.Clear();
+            cmbResult.Text = "";
+        }
+
+        /// <summary>
+        /// 検索ファイル
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbfile_Click(object sender, EventArgs e)
+        {
+            cmbResult.Items.Clear();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(
+                "EXPLORER.EXE", @"C:\");
+        }
+
+
+        /// <summary>
+        /// 不使用ボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AppsWakeUp_Load(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// 検索ディレクトリ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbSerch_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbAppList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cmbResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void tbfile_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
+
+
